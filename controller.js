c90d9d9eda499f41cfd9d66a4b4f2779d4562273
shidel.dev <joeshidel@gmail.com>
2013-12-17 10:29:31 -0500

    auto resizing for iframes

--- controller.js
+++ controller.js
@@ -1,93 +1,81 @@
-if (!window.XMLHttpRequest && 'ActiveXObject' in window) {
-    window.XMLHttpRequest = function() {
-        return new ActiveXObject('MSXML2.XMLHttp');
-    };
-}
+var view = {
+    create: function(routes) {
+        view.routes = routes;
+        var readyStateCheckInterval = setInterval(function() {
+            if (document.readyState === "complete") {
+                clearInterval(readyStateCheckInterval);
+                var hashValue = view.getHash(),
+                    handles = document.querySelectorAll('.handle'),
+                    i,
+                    flag = false;
+                if (hashValue === '') {
+                    view.update(routes.index.name, true);
+                } else {
+                    view.update(view.getHash(), false);
+                }
+                for (i = 0; i < handles.length; i += 1) {
+                    handles[i].addEventListener('click', view.trigger);
+                }
 
-
-
-
-
-window.onload = function() {
-
-
-
-
-
-    if (routes) {
-
-        var View = routes,
-            flag = true,
-            frame = document.getElementById(routes.view);
-        View.update = function(hashValue, flag) {
-            if (routes.method == 'ajax') {
-                frame.innerHTML = "";
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', routes.exceptions[hashValue], true);
-                xhr.onreadystatechange = function() {
-                    if (this.readyState !== 4) return;
-                    if (this.status !== 200) return; // or whatever error handling you want
-                    frame.innerHTML = this.responseText;
+                window.onhashchange = function() {
+                    if (flag) {
+                        view.update(view.getHash(), false);
+                    } else {
+                        flag = true;
+                    }
                 };
-                xhr.send();
 
-            } else {
-                var temp = document.createElement("iframe");
-                parent = frame.parentNode;
-                temp.src = routes.exceptions[hashValue];
-                temp.id = routes.view;
-                parent.replaceChild(temp, frame);
-                frame = temp;
-            }
-            if (flag) {
-                View.setHash(hashValue);
-            }
 
+
+            }
+        }, 10);
+    },
+
+    update: function(hashValue, flag) {
+        var temp = document.createElement("iframe"),
+            frame = document.getElementById(view.routes.view),
+            parent = frame.parentNode;
+        temp.src = view.routes.exceptions[hashValue];
+        temp.id = view.routes.view;
+
+        parent.replaceChild(temp, frame);
+        temp.contentWindow.onresize = function() {
+            view.sizeFrame(view.routes.view);
+            console.log('ok')
         };
-
-
-        View.getHash = function() {
-            return window.location.hash.substring(1);
+        if (temp.contentWindow.readyState == "complete") {
+            view.sizeFrame(view.routes.view);
+        } else {
+            temp.contentWindow.addEventListener("load", function() {
+                setTimeout(view.sizeFrame(view.routes.view), 0);
+            });
         }
 
-
-        View.setHash = function(str) {
-            flag = false;
-            window.location.hash = str;
-        }
-
-
-        window.onhashchange = function(e) {
-            if (flag) {
-                View.update(View.getHash(), false);
-            } else {
-                flag = true;
-            }
-        };
-
-
-        var hashValue = View.getHash(),
-            handles = document.querySelectorAll('.handle');
-
-        for (var i = 0; i < handles.length; i++) {
-            handles[i].addEventListener('click', evTrigger)
-
-
-        }
-
-        function evTrigger() {
-            console.log(this)
-            var link = this.getAttribute('data-link')
-            View.update(link, true)
-            return false;
+        if (flag) {
+            view.setHash(hashValue);
         }
 
 
 
-        if (hashValue == '') {
-            View.update(routes.index.name, true);
-        }else{
-            View.update(View.getHash(), false);   
-        }
+    },
+
+    trigger: function() {
+        var link = this.getAttribute('data-link');
+        view.update(link, true);
+    },
+
+    getHash: function() {
+        return window.location.hash.substring(1);
+    },
+
+    setHash: function(str) {
+        flag = false;
+        window.location.hash = str;
+    },
+
+    sizeFrame: function(elem) {
+        var el = document.getElementById(elem);
+        el.height = (el.contentWindow.document.body.offsetHeight) + "px";
     }
+
 };
